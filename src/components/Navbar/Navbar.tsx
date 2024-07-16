92e1d899f41bceb54fad0e6dbd279fa313ca0644
mostafa <mostafa.kamel@almotahidaeducation.com>
2024-07-16 17:41:18 +0300

    services and projects no breaking

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import Image from "next/image";
 import { routes } from "@/routes";
+import { IoMenu } from "react-icons/io5";
+import { usePathname } from "next/navigation";
 
 import styles from "./navbar.module.scss";
-import { usePathname } from "next/navigation";
 
 type Props = {
   theme: string | "";
@@ -13,25 +14,45 @@ const Navbar = (props: Props) => {
   const pathname = usePathname();
   const { theme } = props;
 
+  const [show, setShow] = React.useState(false);
+
   return (
-    <nav className={styles.nav}>
-      <Image
-        src={theme === "light" ? "/dark-logo.svg" : "/light-logo.svg"}
-        alt="logo"
-        width={250}
-        height={64}
-      />
-      <ul>
-        {routes.map(route => (
-          <li key={route.href}>
-            <a href={route.href} className={pathname === route.href ? styles.active : ""}>
-              {route.label}
-            </a>
-          </li>
-        ))}
-      </ul>
-      <button className={styles[`${theme}`]}>Become a client</button>
-    </nav>
+    <>
+      <nav className={styles.nav}>
+        <div>
+          <Image
+            src={theme === "light" ? "/dark-logo.svg" : "/light-logo.svg"}
+            alt="logo"
+            width={250}
+            height={64}
+          />
+          <ul className={styles['main-menu']}>
+            {routes.map(route => (
+              <li key={route.href}>
+                <a href={route.href} className={pathname === route.href ? styles.active : ""}>
+                  {route.label}
+                </a>
+              </li>
+            ))}
+          </ul>
+          <button className={`${styles.button} ${styles[`${theme}`]}`}>Become a client</button>
+          <button className={styles.hamburger} onClick={() => setShow(prevState => !prevState)}>
+            <IoMenu />
+          </button>
+        </div>
+        {show && (
+          <div className={styles.menu}>
+            {routes.map(route => (
+              <li key={route.href}>
+                <a href={route.href} className={pathname === route.href ? styles.active : ""}>
+                  {route.label}
+                </a>
+              </li>
+            ))}
+          </div>
+        )}
+      </nav>
+    </>
   );
 };
 
